d209f7d19f3cb1d19141480c46ae774d4422a456
nooraiym <140681537+nooraiym@users.noreply.github.com>
2024-10-24 17:54:54 +0500

    fix: bug w/buttons and products mock data

--- src/mock/products.ts
+++ src/mock/products.ts
@@ -2,6 +2,7 @@ export type Product = {
   name: string;
   description: string;
   imageUrl: string;
+  cost: number;
   categoryId: number;
 };
 
@@ -12,6 +13,7 @@ export const products = [
       'Горячий сытный омлет с поджаристой корочкой, ветчина, шампиньоны и моцарелла',
     imageUrl:
       'https://media.dodostatic.net/image/r:292x292/11EE7970321044479C1D1085457A36EB.webp',
+    cost: 240,
     categoryId: 2,
   },
   {
@@ -20,6 +22,7 @@ export const products = [
       'Сытный и сбалансированный завтрак — омлет с поджаристой корочкой, пикантная пепперони, томаты и моцарелла',
     imageUrl:
       'https://media.dodostatic.net/image/r:292x292/11EE94ECF33B0C46BA410DEC1B1DD6F8.webp',
+    cost: 230,
     categoryId: 2,
   },
   {
@@ -28,6 +31,7 @@ export const products = [
       'Когда хочется нежную молочную пенку, на помощь приходит классический латте',
     imageUrl:
       'https://media.dodostatic.net/image/r:292x292/11EE7D61B0C26A3F85D97A78FEEE00AD.webp',
+    cost: 195,
     categoryId: 2,
   },
   {
@@ -36,6 +40,7 @@ export const products = [
       'Поджаристая чиабатта и знакомое сочетание, цыпленка, ветчины из цыпленка, моцареллы со свежими томатами и соусом ранч',
     imageUrl:
       'https://media.dodostatic.net/image/r:292x292/11EE796FF0059B799A17F57A9E64C725.webp',
+    cost: 250,
     categoryId: 3,
   },
   {
@@ -43,6 +48,7 @@ export const products = [
     description: 'Хрустящие куриные наггетсы из цыпленка в панировке',
     imageUrl:
       'https://media.dodostatic.net/image/r:292x292/11EE7D618B5C7EC29350069AE9532C6E.webp',
+    cost: 195,
     categoryId: 3,
   },
   {
@@ -50,6 +56,7 @@ export const products = [
     description: 'Запеченная в печи картошечка. Привычный вкус и мало масла',
     imageUrl:
       'https://media.dodostatic.net/image/r:292x292/11EED646A9CD324C962C6BEA78124F19.webp',
+    cost: 195,
     categoryId: 3,
   },
   {
@@ -58,6 +65,7 @@ export const products = [
       'Легендарная горячая закуска с цыплёнком, томатами, моцареллой, соусом ранч в тонкой пшеничной лепешке',
     imageUrl:
       'https://media.dodostatic.net/image/r:292x292/11EE796F96D11392A2F6DD73599921B9.webp',
+    cost: 395,
     categoryId: 3,
   },
   {
@@ -66,6 +74,7 @@ export const products = [
       'Горячая закуска с цыпленком, перцем халапеньо, маринованными огурчиками, томатами, моцареллой и соусом барбекю в тонкой пшеничной лепешке',
     imageUrl:
       'https://media.dodostatic.net/image/r:292x292/11EE796FD3B594068F7A752DF8161D04.webp',
+    cost: 395,
     categoryId: 3,
   },
   {
@@ -74,6 +83,7 @@ export const products = [
       'Сливочная прохлада классического молочного коктейля с добавлением банана',
     imageUrl:
       'https://media.dodostatic.net/image/r:292x292/11EEE20B8772A72A9B60CFB20012C185.webp',
+    cost: 210,
     categoryId: 4,
   },
   {
@@ -82,6 +92,7 @@ export const products = [
       'Не важно, какое время года на улице, этот коктейль с карамельным концентратом вернет вас в осень с одного глотка',
     imageUrl:
       'https://media.dodostatic.net/image/r:292x292/11EE79702E2A22E693D96133906FB1B8.webp',
+    cost: 230,
     categoryId: 4,
   },
   {
@@ -90,6 +101,7 @@ export const products = [
       'Как вкуснее есть печенье? Его лучше пить! Попробуйте молочный коктейль с мороженым и дробленым печеньем «Орео»',
     imageUrl:
       'https://media.dodostatic.net/image/r:292x292/11EE796FA1F50F8F8111A399E4C1A1E3.webp',
+    cost: 210,
     categoryId: 4,
   },
   {
@@ -98,6 +110,7 @@ export const products = [
       'В мире так много коктейлей, но классика — вечна. Попробуйте наш молочный напиток с мороженым',
     imageUrl:
       'https://media.dodostatic.net/image/r:292x292/11EE796F93FB126693F96CB1D3E403FB.webp',
+    cost: 190,
     categoryId: 4,
   },
   {
@@ -106,6 +119,7 @@ export const products = [
       'Насыщенный кофе с бархатистой пенкой, дополненный мягкими нотками ирландского крема для изысканного вкуса',
     imageUrl:
       'https://media.dodostatic.net/image/r:292x292/11EE7D61999EBDA59C10E216430A6093.webp',
+    cost: 205,
     categoryId: 5,
   },
   {
@@ -114,6 +128,7 @@ export const products = [
       'Если не шоколад, то карамель! А капучино с карамельным сиропом особенно хорош',
     imageUrl:
       'https://media.dodostatic.net/image/r:292x292/11EE7D61AED6B6D4BFDAD4E58D76CF56.webp',
+    cost: 195,
     categoryId: 5,
   },
   {
@@ -122,6 +137,7 @@ export const products = [
       'Много молока и кокоса. Уютный кокосовый латте на основе эспрессо',
     imageUrl:
       'https://media.dodostatic.net/image/r:292x292/11EE7D61B19FA07090EE88B0ED347F42.webp',
+    cost: 195,
     categoryId: 5,
   },
   {
@@ -129,6 +145,7 @@ export const products = [
     description: 'Горячий напиток на основе эспрессо',
     imageUrl:
       'https://media.dodostatic.net/image/r:292x292/11EE7D61B044583596548A59078BBD33.webp',
+    cost: 95,
     categoryId: 5,
   },
   {
@@ -137,6 +154,7 @@ export const products = [
       'Когда хочется нежную молочную пенку, на помощь приходит классический латте',
     imageUrl:
       'https://media.dodostatic.net/image/r:292x292/11EE7D61B0C26A3F85D97A78FEEE00AD.webp',
+    cost: 195,
     categoryId: 5,
   },
 ];
