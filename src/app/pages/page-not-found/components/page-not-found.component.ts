c36d35783eb1afb645a96088e0f843b47114a0a9
nooraiym <140681537+nooraiym@users.noreply.github.com>
2024-10-29 13:41:41 +0500

    feat: ad back action to 404

--- src/app/pages/page-not-found/components/page-not-found.component.ts
+++ src/app/pages/page-not-found/components/page-not-found.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
   imports: [],
   templateUrl: './page-not-found.component.html',
-  styleUrl: './page-not-found.component.scss'
+  styleUrl: './page-not-found.component.scss',
 })
 export class PageNotFoundComponent {
+  private router = inject(Router);
 
+  onClick() {
+    this.router.navigate(['']);
+  }
 }
