2f4c6639a2620fa7fa92ca6ddca3925a12b82b4b
nooraiym <140681537+nooraiym@users.noreply.github.com>
2024-11-01 18:04:13 +0500

    feat: add side-cart layout

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,14 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { SearchInputComponent } from './search-input/search-input.component';
+import { SideCartComponent } from "./side-cart/side-cart.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, SearchInputComponent],
+  imports: [CommonModule, RouterLink, SearchInputComponent, SideCartComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
   @Input({ required: true }) mode: 'main' | 'profile' = 'main';
+  isCartOpen = false;
+
+  toggleCart() {
+    this.isCartOpen = !this.isCartOpen;
+  }
+
+  closeCart() {
+    this.isCartOpen = false;
+  }
 }
